eeee5643136eb63987cbfccb004b1be92b894758
themoffat28 <bthemoffat@gmail.com>
2016-09-16 07:46:33 -0600

    Started Project, created ignore file, and provided examples of all questions.

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+// String Change
+var powerOnWelcome = "What do you want"
+powerOnWelcome = "Hello valued user!"
+
+//String Interpolation
+let hundo = 100
+let boss = "Braeden"
+let time = "second"
+let fact = "My name is \(boss) and I make $\(hundo) every \(time)."
+print (fact)
+
+//Counting Characters
+let name = "Braeden Moffat"
+let countingCharacters = "My name Braeden Moffat has \(name.characters.count - 1) characters."
+print(countingCharacters) 
+
+//Comparing Strings
+
+let var1 = "cat"
+let var2 = "cat"
+var1 == var2
